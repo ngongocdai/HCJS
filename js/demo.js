0ab26e2c845410382847e00f1ad045c35c6174a1
ngongocdai <115610459+ngongocdai@users.noreply.github.com>
2022-12-02 10:39:41 +0700

    up

---
+++ js/demo.js
@@ -0,0 +1,93 @@
+//code js
+var x = 10;
+//let y = 20;
+x = "hello";// dynamic datatype
+var t = true;
+console.log(x);// print
+
+var y = 20;
+
+var z = y+10;
+var k = x+y;
+var u = y+x;//20hello
+var o = y+"world";//20world
+var p = "hello"+ "world"+ y;//helloworld20
+var a="20"+20;//2010
+
+console.log(a);
+console.log(y);
+var b = 5 + 10 + 15 +"a";// 30a
+if(y>10){
+
+}else if(y>15){
+
+}else{
+
+}
+for(var j=0;j<10;j++){
+    console.log("j="+j);
+}
+console.log("xin chao cac ban"+20+", chuc buoi sang tot lanh");
+// gia tri cu Y=20, gia tri dep
+console.log("gia tri cua y"+y+,"gia tri dep");
+console.log(gia tri cua y=$(y),gia tri dep);
+
+// khai bao 1 ham
+function tinh tong(a,b){
+// return a+b
+console.log(a+b);
+}
+var kq = tinhtong(4,5);
+
+var arr =[];
+arr[0]=15;
+arr[1]="abc";
+arr[2]=true;
+arr.push(17);
+arr.push("xyz");
+arr.push("uio");
+
+for(var i=0;i<arr.length;i++){
+    console.log(arr[i]);
+}
+
+//alert("Alert");
+
+//var rs = confirm("ban chac chan muon nop bai");//return baolean
+//if (rs == true){
+  //  console.log("nguoi dung da dong y");
+//}else{
+ //   console.log("huy bo chien dich");
+//}
+
+//var s = prompt ("vui long nhap ho ten");// return string
+//console.log(s);
+// neu nhap -> string
+// neu ko nhap + ok-> string rong
+// neu nhan cancel->null
+
+//setTimeout(function (){
+ //   alert("hello");
+//},3000);
+
+span_min =document.getElementById("minutes");
+span_sec =document.getElementById("seconds");
+var min=10,sec=0;
+var timer setInterval(function(){
+    var m = min;
+    if(min<10){
+        m = "0"+min;
+    }
+    var s = sec<10?"0"+sec:sec;// toan tu 3 ngoi
+    span_min.innerText = m;
+    span_sec.innerText = s;
+    //console.log(min+":"+s);
+    sec--;
+    if(sec<0){
+        sec = 59;
+        min--;
+    }
+    if(min < 0){
+        clearInterval(timer);
+    }
+},100);
